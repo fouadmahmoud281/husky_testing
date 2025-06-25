b33da1c7af9426eb09a7d1b6968141949f8a21ef
fouad mahmoud <100407343+fouadmahmoud281@users.noreply.github.com>
2025-06-25 13:32:58 +0300

    Fix button syntax error

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         </div>
         <button className="App-button" onClick={() => alert('Hello from React!')}>
           Click Me!
-        </butto
+        </button>
       </header>
     </div>
   );
